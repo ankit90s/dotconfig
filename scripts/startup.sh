c9b78ab32ab7f90524e215bd89867eef5426460d
ankit90s <ak7591527@gmail.com>
2023-09-08 08:08:35 +0530

    updates

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-mpv --no-video /home/$USER/.config/conky/start-computeraif-14572.mp3
+mpv --no-video /home/$USER/.config/sound/start-computeraif-14572.mp3
 
 killall conky && sleep 2s
-conky -c /home/$USER/.config/conky/conky1.conkyrc
+conky -c /home/$USER/.config/conky/SP-01/SP-01.conf
