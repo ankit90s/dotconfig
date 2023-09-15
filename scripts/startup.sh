2f64eed6c76efa0dc0d3024ed59cd537f2e4c883
ankit90s <ak7591527@gmail.com>
2023-09-15 17:23:21 +0530

    updates

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,5 +2,5 @@
 
 mpv --no-video /home/$USER/.config/sound/start-computeraif-14572.mp3
 
-killall conky && sleep 2s
-conky -c /home/$USER/.config/conky/Regulus/Regulus.conf
+# killall conky && sleep 2s
+# conky -c /home/$USER/.config/conky/Regulus/Regulus.conf
