938213e3fa29296f263caf2c7747db18ca714c3e
ankit <ankit@localhost.localdomain>
2023-12-20 12:47:48 +0530

    updates

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-mpv --no-video /home/$USER/.config/sound/start-computeraif-14572.mp3
+mpv --no-video /home/$USER/.config/sound/windows_11_startup.mp3
 
 # killall conky && sleep 2s
 # conky -c /home/$USER/.config/conky/Regulus/Regulus.conf
