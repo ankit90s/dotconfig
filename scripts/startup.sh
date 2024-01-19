bf032bc5a61498de468b7560dafe450249bffdd4
ankit <ankit@localhost.localdomain>
2024-01-19 22:32:23 +0530

    updates

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,4 +3,4 @@
 mpv --no-video /home/$USER/.config/sound/windows_11_startup.mp3
 
 # killall conky && sleep 2s
-# conky -c /home/$USER/.config/conky/Regulus/Regulus.conf
+conky -c /home/$USER/.config/conky/conky1.conkyrc
